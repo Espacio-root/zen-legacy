5a87a8ed89009f94a50a3cdef44c157bb5697127
Espacio-root <Abdullahd00124@gmail.com>
2023-10-14 02:06:57 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from mitmproxy import http
+import re
+
+class Blocker:
+
+    def __init__(self):
+        self.fp = 'blocklist.txt'
+        self.redirect_url = 'https://www.google.com/search?q=inspiring+quotes+for+entrepreneurs&tbm=isch'
+
+        assert self.fp, 'Blocklist file not found'
+        self._process_blocklist()
+
+    def _process_blocklist(self):
+        with open(self.fp, 'r') as f:
+            self.redirects = [f'www.{line.strip()}' for line in f.readlines()]
+    
+    def request(self, flow: http.HTTPFlow):
+        if flow.request.host in self.redirects:
+            flow.request.url = self.redirect_url
+
+addons = [
+    Blocker()
+]
